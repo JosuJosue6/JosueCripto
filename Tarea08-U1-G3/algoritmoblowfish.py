c2d212db8fc6aaad55f5b0c84f8ef7ba137794a0
JosuJosue <josuensd@gmail.com>
2024-05-15 00:28:03 -0500

    Actualizacion

--- Tarea08-U1-G3/algoritmoblowfish.py
+++ Tarea08-U1-G3/algoritmoblowfish.py
@@ -9,52 +9,58 @@ def leer_txt(documento):
 
 # Blowfish utiliza claves de 32 a 448 bits, aquí usamos 64 bits (8 bytes)
 def generar_llave():
-    return os.urandom(8)  
+    return os.urandom(56)  
 
 def texto_cifrado(texto_plano, llave):
-    cipher = Blowfish.new(llave, Blowfish.MODE_ECB)
-    return cipher.encrypt(pad(texto_plano.encode(), Blowfish.block_size))
+    texto_plano = str(texto_plano)
+    cifrar = Blowfish.new(llave, Blowfish.MODE_ECB)
+    return cifrar.encrypt(pad(texto_plano.encode('utf-8'), Blowfish.block_size))
 
 def texto_descifrado(texto_cifrado, llave):
-    cipher = Blowfish.new(llave, Blowfish.MODE_ECB)
-    return unpad(cipher.decrypt(texto_cifrado), Blowfish.block_size).decode()
+    cifrar = Blowfish.new(llave, Blowfish.MODE_ECB)
+    return unpad(cifrar.decrypt(texto_cifrado), Blowfish.block_size).decode('utf-8')
 
-def contar_caracteres(archivo):
-    with open(archivo, 'r') as txt:
-        while True:
-            caracter = txt.read(1)
-            if not caracter:
-                break
-            print(caracter)
+def contar_caracteres(texto):
+    texto_sin_espacios = str(texto).replace(" ", "")
+    return len(texto_sin_espacios)
 
-
+def contar_caracteres2(texto):
+    return len(texto)
 
 def main():
     tiempo_inicio = time.time()
     documento = '10.txt'
-    texto_plano = leer_txt(documento)
-    print(f'\nTiempo para leer el archivo: {time.time() - tiempo_inicio} segundos')
 
-    tiempo_inicio = time.time()
-    llave = generar_llave()
-    print(f'\nClave generada: {llave}')
-    print(f'Tiempo para generar la clave: {time.time() - tiempo_inicio} segundos')
+    if not os.path.exists(documento):
+        print(f"\nEl archivo {documento} no se encuentra. Cambie el nombre del archivo o verifique la ruta.")
+        return None
+    
+    else:
 
-    tiempo_inicio = time.time()
-    txt_cifrado = texto_cifrado(texto_plano, llave)
-    print(f'\nTexto cifrado: {txt_cifrado}')
-    print(f'Tiempo para cifrar el texto: {time.time() - tiempo_inicio} segundos')
+        texto_plano = leer_txt(documento)
+        print(f'\nDocumento de: {documento.split(".")[0] } palabras')
+        print(f'\nTiempo para leer el archivo: {(time.time() - tiempo_inicio)* 1000} milisegundos')
 
-    tiempo_inicio = time.time()
-    txt_descifrado = texto_descifrado(txt_cifrado, llave)
-    print(f'\nTexto descifrado: {txt_descifrado}')
-    print(f'Tiempo para descifrar el texto: {time.time() - tiempo_inicio} segundos')
+        tiempo_inicio = time.time()
+        llave = generar_llave()
+        print(f'\nClave generada: {llave}')
+        print(f'Tiempo para generar la clave: {(time.time() - tiempo_inicio)* 1000} milisegundos')
 
-    caracter_entrada = contar_caracteres(documento)
-    print(f'\nCaracteres en el archivo: {caracter_entrada}')
+        tiempo_inicio = time.time()
+        txt_cifrado = texto_cifrado(texto_plano, llave)
+        #print(f'\nTexto cifrado: {txt_cifrado}')
+        print(f'Tiempo para cifrar el texto: {(time.time() - tiempo_inicio)* 1000} milisegundos')
 
-    caracter_salida = contar_caracteres(txt_cifrado)
-    print(f'\nCaracteres en el archivo cifrado: {caracter_salida}')
+        tiempo_inicio = time.time()
+        txt_descifrado = texto_descifrado(txt_cifrado, llave)
+        #print(f'\nTexto descifrado: {txt_descifrado}')
+        print(f'Tiempo para descifrar el texto: {(time.time() - tiempo_inicio)* 1000} milisegundos')
+
+        caracter_entrada = contar_caracteres(texto_plano)
+        print(f'\nCaracteres en el archivo: {caracter_entrada}')
+
+        caracter_salida = contar_caracteres2(txt_cifrado)
+        print(f'\nCaracteres en el archivo cifrado: {caracter_salida}\n')
 
 if __name__ == '__main__':
     main()
